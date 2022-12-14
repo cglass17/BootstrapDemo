81fca4aab966329ed8c718390163dd94b819f661
cglass17 <cglass17@hotmail.com>
2022-12-14 06:55:41 -0500

    4 files modified

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Import stylesheets
 import './style.css';
+// Import Jquery
+import $ from 'jquery';
+//Import Bootstrap
+import 'bootstrap';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+$(document).ready(function () {
+  $("[data-bs-toggle='dropdown']").each(function (index) {
+    $(this).mouseenter(function (event) {
+      event.stopPropagation();
+      $(this).parent().children().addClass('show');
+    });
+  });
+});
